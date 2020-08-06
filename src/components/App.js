cf51f23a8640e55adaea01d29a38ac721ddefaa7
Andreea Ros <ros.andreeacatalina@gmail.com>
2020-08-06 20:41:54 +0100

    First commit.

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{ borderStyle: "solid" }}> navbar </div>
+      <div style={{ borderStyle: "solid" }}> sidebar </div>
+      <div style={{ borderStyle: "solid" }}> main </div>
+    </div>
+  );
+}
+
+export default App;
