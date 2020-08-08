09fb00f1e9afee967c8bb6ac0e0c9b461c3fa268
Andreea Ros <ros.andreeacatalina@gmail.com>
2020-08-08 15:11:14 +0100

    Ordered tasks by deadline in TaskList.

--- src/components/task-list.js
+++ src/components/task-list.js
@@ -4,15 +4,21 @@ import Task from "./task";
 const TaskList = ({ tasks }) => {
   return (
     <div className="TaskList" data-testid="task-list-test">
-      {tasks.map((task, i) => (
-        <Task
-          id={task.id}
-          key={task.id}
-          description={task.description}
-          deadline={task.deadline}
-          index={i}
-        />
-      ))}
+      {tasks
+        .sort(
+          (a, b) =>
+            parseInt(a.deadline.slice(0, 16).replace(/\D/g, "")) -
+            parseInt(b.deadline.slice(0, 16).replace(/\D/g, ""))
+        )
+        .map((task, i) => (
+          <Task
+            id={task.id}
+            key={task.id}
+            description={task.description}
+            deadline={task.deadline}
+            index={i}
+          />
+        ))}
     </div>
   );
 };
--- src/components/task.js
+++ src/components/task.js
@@ -3,14 +3,18 @@ import moment from "moment";
 
 const Task = (props) => {
   const { description, deadline } = props;
-  const finalDate = moment(deadline).format("ddd MMM DD");
+  const finalDate = moment(deadline).format("ddd MMM DD YYYY");
+
   return (
-    <div className="task" data-testid={`task-${props.index}`}>
-      {/*  "task:" = description element (input element styled as static)  */}
-      <div className="task-description"> task: {description}</div>
-      {/* "due:" = deadline element (input element styled as static)  */}
-      <div className="task-deadline">due: {finalDate} </div>
-    </div>
+    <>
+      {console.log(deadline.slice(0, 16).replace(/\D/g, ""))}
+      <div className="task" data-testid={`task-${props.index}`}>
+        {/*  "task:" = description element (input element styled as static)  */}
+        <div className="task-description"> task: {description}</div>
+        {/* "due:" = deadline element (input element styled as static)  */}
+        <div className="task-deadline">due: {finalDate} </div>
+      </div>
+    </>
   );
 };
 
